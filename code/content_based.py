078944237433309ae9453c05a87958e832f96a68
Ricardo Santos <r_patricio@live.com.pt>
2022-07-13 19:25:39 +0200

    Update content_based.py

--- code/content_based.py
+++ code/content_based.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 import plotly.graph_objects as go
 
-@st.cache
+
 def get_allstats(minutes):
     # cargamos todos los datos estadisticos relativos a las distintas caracteristicas de los jugadores
     def_df_full = pd.read_csv("./data/defensive_2022.csv")
