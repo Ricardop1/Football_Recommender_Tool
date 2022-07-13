490f5ab32b40a8c9e9669d3dc4b39a6598431e89
Ricardo Santos <r_patricio@live.com.pt>
2022-07-13 18:29:58 +0200

    Update collaborative_filtering.py

--- code/collaborative_filtering.py
+++ code/collaborative_filtering.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from ast import literal_eval
-from .content_based import get_allstats
+from content_based import get_allstats
 
 
 def clean_clubs(clubs):
