e8142a36e4681e50dee82d0278f979e95c8c9a9f
Ricardo Santos <r_patricio@live.com.pt>
2023-01-14 19:10:28 +0100

    Update content_based.py

--- code/content_based.py
+++ code/content_based.py
@@ -111,9 +111,9 @@ def plot_similar_players(player1, player2, df):
 def plot_similar_players_pizza(player1, player2, df):
     params = df.iloc[:, 1:].columns.values.tolist()
     player1_stats = df[df["Player"] == player1]
-    player1_stats = player1_stats.iloc[:, 1:].copy().values
+    player1_stats = player1_stats.iloc[:, 1:].copy().values.tolist()
     player2_stats = df[df["Player"] == player2]
-    player2_stats = player2_stats.iloc[:, 1:].copy().values
+    player2_stats = player2_stats.iloc[:, 1:].copy().values.tolist()
 
     font_normal = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/roboto/'
                               'Roboto%5Bwdth,wght%5D.ttf')
