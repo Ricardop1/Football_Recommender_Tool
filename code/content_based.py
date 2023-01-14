56834a6a0336c9337dce35fad230993c1fe63838
Ricardo Santos <r_patricio@live.com.pt>
2023-01-14 19:12:39 +0100

    Update content_based.py

--- code/content_based.py
+++ code/content_based.py
@@ -111,9 +111,9 @@ def plot_similar_players(player1, player2, df):
 def plot_similar_players_pizza(player1, player2, df):
     params = df.iloc[:, 1:].columns.values.tolist()
     player1_stats = df[df["Player"] == player1]
-    player1_stats = player1_stats.iloc[:, 1:].copy().values.tolist()
+    player1_stats = player1_stats.iloc[:, 1:].copy().values.tolist()[0]
     player2_stats = df[df["Player"] == player2]
-    player2_stats = player2_stats.iloc[:, 1:].copy().values.tolist()
+    player2_stats = player2_stats.iloc[:, 1:].copy().values.tolist()[0]
     st.write(params)
     st.write(player1_stats)
 
