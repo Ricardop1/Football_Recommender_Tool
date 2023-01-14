8b293f9fa1e751d047e23a6d5758f1d9663d9c42
Ricardo Santos <r_patricio@live.com.pt>
2023-01-14 19:29:36 +0100

    Update content_based.py

--- code/content_based.py
+++ code/content_based.py
@@ -111,9 +111,9 @@ def plot_similar_players(player1, player2, df):
 def plot_similar_players_pizza(player1, player2, df):
     params = df.iloc[:, 1:].columns.values.tolist()
     player1_stats = df[df["Player"] == player1]
-    player1_stats = player1_stats.iloc[:, 1:].copy().values
+    player1_stats = player1_stats.iloc[:, 1:].copy().values.squeeze()
     player2_stats = df[df["Player"] == player2]
-    player2_stats = player2_stats.iloc[:, 1:].copy().values
+    player2_stats = player2_stats.iloc[:, 1:].copy().values.squeeze()
     #min_range = [i / 2 for i in player1_stats]
     min_range = player1_stats / 2
     max_range = player1_stats * 2
