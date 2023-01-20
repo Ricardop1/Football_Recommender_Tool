a210998162a6d16b022068e14563e95a980fe979
Ricardo Santos <r_patricio@live.com.pt>
2023-01-20 18:25:17 +0100

    plot

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -81,9 +81,9 @@ elif "select_team" in st.session_state and \
     df_with_team = get_scaled_df_with_team(position, st.session_state.select_team, all_stats)
     player_plot = st.selectbox("Select Player to Visualize", jugadores, key="select_player_visualize2")
     fig_team = plot_similar_players(player_plot, st.session_state.select_team, df_with_team)
-    #fig_play2 = plot_similar_players_pizza(player_plot, st.session_state.select_team, df_with_team)
+    fig_play2 = plot_similar_players_pizza(player_plot, st.session_state.select_team, df_with_team)
     st.plotly_chart(fig_team, use_container_width=True)
-    #st.write(fig_play2)
+    st.write(fig_play2)
 elif "select_team" in st.session_state and \
         select_type == "Best players to fit a Team" and \
         st.session_state.select_team != "Select Option":
