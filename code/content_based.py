c52e0074ad720fdb5f68654ca0ab6eb2978b4ea9
Ricardo Santos <r_patricio@live.com.pt>
2023-01-21 12:06:01 +0100

    Update content_based.py

--- code/content_based.py
+++ code/content_based.py
@@ -216,7 +216,7 @@ def get_recommendations_by_player(name_player, df, number_of_recommendations):
 
 
 def get_positions_df_national_team(all_stats):
-    st.write(all_stats.head(3))
+
     high_corr_df_cb = np.unique(DF_CB_COLS)
     high_corr_df_db = np.unique(DF_DB_COLS)
 
@@ -226,7 +226,7 @@ def get_positions_df_national_team(all_stats):
 
     high_corr_fw_aw = np.unique(FW_AW_COLS)
     high_corr_fw_st = np.unique(FW_ST_COLS)
-
+    st.write(all_stats["Comp"].head(3))
     defensive_df_cb = all_stats[list(high_corr_df_cb) + ["Player", "Squad", "Pos", "Nation","Comp"]]
     defensive_df_cb = defensive_df_cb[defensive_df_cb["Pos"] == "DF"]
     defensive_df_db = all_stats[list(high_corr_df_db) + ["Player", "Squad", "Pos", "Nation","Comp"]]
